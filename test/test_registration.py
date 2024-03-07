668cdcfef1ffed4a31060a6ba726f85ab187ec2f
Svetlana Nikel <mzsveta@mail.ru>
2024-03-07 08:39:18 +0500

    new_commit_2

--- test/test_registration.py
+++ test/test_registration.py
@@ -1,3 +1,4 @@
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -9,7 +10,8 @@ class TestRegistration:
         driver.find_element(*My_locators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(My_locators.REGISTER_URL_AUTH)).click()
         driver.find_element(*My_locators.NAME_INPUT_FIELD_REG).send_keys('Sveta')
-        driver.find_element(*My_locators.EMAIL_INPUT_FIELD_REG).send_keys('svetlana_nikel_6_124@yandex.ru')
+        random_email = f'svetlana_nikel_6_{random.randint(100, 999)}'
+        driver.find_element(*My_locators.EMAIL_INPUT_FIELD_REG).send_keys(random_email)
         driver.find_element(*My_locators.PASSWORD_INPUT_FIELD_REG).send_keys(123458)
         driver.find_element(*My_locators.REGISTRATION_BUTTON_REG).click()
 
@@ -17,7 +19,8 @@ class TestRegistration:
         driver.find_element(*My_locators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(My_locators.REGISTER_URL_AUTH)).click()
         driver.find_element(*My_locators.NAME_INPUT_FIELD_REG).send_keys('Sveta')
-        driver.find_element(*My_locators.EMAIL_INPUT_FIELD_REG).send_keys('svetlana_nikel_6_124@yandex.ru')
+        random_email = f'svetlana_nikel_6_{random.randint(100, 999)}'
+        driver.find_element(*My_locators.EMAIL_INPUT_FIELD_REG).send_keys(random_email)
         driver.find_element(*My_locators.PASSWORD_INPUT_FIELD_REG).send_keys(12345)
         driver.find_element(*My_locators.REGISTRATION_BUTTON_REG).click()
         assert driver.find_element(*My_locators.INCORRECT_PASSWORD_ERROR_REG)
